1755e439426acedbc826445acbb693812685a360
DF <a2470abc@gmail.com>
2018-09-15 22:32:27 +0800

    alpha version done
    Todo:
    optmize exit behavior

---
+++ steam_faker/steam_faker/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main(void)
+{
+	char cTmp[50];
+	memset(cTmp, 0, 50);
+	std::cout << "Steam_faker starts successfully\n";
+	cout << "input any letter and click ENTER¡@to exit Steam_Faker\n";
+	if( ( cin >> cTmp ))
+	{
+		return 0;
+
+	}
+	
+	
+
+
+}
