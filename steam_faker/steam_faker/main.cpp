ae8854d15d3064224d40f4c22dc4f153cc3219f8
DF <a2470abc@gmail.com>
2018-09-17 10:56:34 +0800

    adjust description

--- steam_faker/steam_faker/main.cpp
+++ steam_faker/steam_faker/main.cpp
@@ -8,7 +8,6 @@ int main(void)
 	char cTmp[50];
 	memset(cTmp, 0, 50);
 	std::cout << "Steam_faker starts successfully\n";
-	cout << "input any letter and click ENTER to exit Steam_Faker\n";
 	system("pause");
 	/*
 	if( ( cin >> cTmp ))
