8c0587a965e236bbaf4cb2c4bdc1df5e25438246
df <a2470abc@gmail.com>
2018-09-17 10:38:43 +0800

    adjust the way to exit programe

--- steam_faker/steam_faker/main.cpp
+++ steam_faker/steam_faker/main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 
+
 using namespace std;
 int main(void)
 {
 	char cTmp[50];
 	memset(cTmp, 0, 50);
 	std::cout << "Steam_faker starts successfully\n";
-	cout << "input any letter and click ENTER¡@to exit Steam_Faker\n";
+	cout << "input any letter and click ENTER to exit Steam_Faker\n";
+	system("pause");
+	/*
 	if( ( cin >> cTmp ))
 	{
 		return 0;
 
 	}
 	
-	
+	*/
 
 
 }
